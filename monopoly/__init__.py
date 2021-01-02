4cb91285434e6e40cd9d1dc698eb84abebace824
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-01-02 17:00:12 -0500

    removed cors

--- monopoly/__init__.py
+++ monopoly/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS
+
 
 
 db = SQLAlchemy()
@@ -25,7 +25,6 @@ def create_app():
     migrate.init_app(app,db)
     ma.init_app(app)
     jwt.init_app(app)
-    cors = CORS(app, resources={r"*": {"origins": app.config["CROSS_ORIGINS"]}})
 
     @app.route('/')
     def index():
