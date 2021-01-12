deeaef12f55b26bc92a9503a12185c2bd8ff14c5
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-01-11 23:29:05 -0500

    added basic page

--- monopoly-ui/src/components/GameSelect.js
+++ monopoly-ui/src/components/GameSelect.js
@@ -1,11 +1,17 @@
-import './GameSelect.css';
+import './GameSelect.css'
 import logo from '../assets/img/main-logo.png'
+
 
 function GameSelect() {
   return (
-    <div>
-        <img src={logo}></img>
+    <div className='game-select'>
+        <img className='game-select__image' src={logo}></img>
+        <div className='game-select__content'>
+              <button className='game-select-content__button'>New Game</button>
+              <button className='game-select-content__button'>Join Game</button>
+        </div>
     </div>
+    
   );
 }
 
