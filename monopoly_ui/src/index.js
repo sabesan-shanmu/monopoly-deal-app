4ee6bd82a237b33ea3f35ba5c89a9ac2379331eb
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-01-17 15:02:09 -0500

    updates

--- monopoly_ui/src/index.js
+++ monopoly_ui/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js'
 import './index.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
