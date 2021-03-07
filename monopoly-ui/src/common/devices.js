9dae6bf6effae5a9eff7b2f0ea4270e0aee2e1e1
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-03-06 23:04:11 -0500

    fixed typo

--- monopoly-ui/src/common/devices.js
+++ monopoly-ui/src/common/devices.js
@@ -2,5 +2,5 @@ export const device = {
     mobile: `only screen and (max-width: 480px)`,
     tablet: `(min-width: 481px) and (max-width: 768px)`,
     laptop: `(min-width: 769px ) and (max-width: 1024px)`,
-    dektop: `only screen and (min-width : 1025px)`
+    desktop: `only screen and (min-width : 1025px)`
 };
