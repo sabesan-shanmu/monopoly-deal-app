aab38ba6279378b7d085325d8cce28fec383f4ed
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-10-04 00:35:33 -0400

    moved images

--- monopoly-api/upload-images.py
+++ monopoly-api/upload-images.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 import pyrebase
-
+from uuid import uuid4
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 firebase_storage = os.environ.get("STORAGE")
+
 
 
 
@@ -19,31 +20,53 @@ firebaseConfig = {
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 storage = firebase.storage()
+auth = firebase.auth()
 
-source_folder = "property-cards\\"
+uid = str(uuid4())
+custom_token = auth.create_custom_token(uid)
+
+
+source_folder = "..\\monopoly-cards\\property-cards\\"
+property_cards = []
+propert_card_id = 1
 for file_name in os.listdir(source_folder):
     file_destination = source_folder+file_name
     file_source = os.path.join(basedir,source_folder+file_name)
     storage.child("property-cards/"+file_name).put(file_source)
+    url = storage.child("property-cards/"+file_name).get_url(custom_token)
+    #property_cards.append()
+    print(url)
 
 
-source_folder = "rent-cards\\"
+source_folder = "..\\monopoly-cards\\rent-cards\\"
+rent_cards = []
+rent_card_id = 1
 for file_name in os.listdir(source_folder):
     file_destination = source_folder+file_name
     file_source = os.path.join(basedir,source_folder+file_name)
     storage.child("rent-cards/"+file_name).put(file_source)
+    url = storage.child("property-cards/"+file_name).get_url(custom_token)
+    print(url)
 
 
-source_folder = "action-cards\\"
+source_folder = "..\\monopoly-cards\\action-cards\\"
+action_cards = []
+action_card_id = 1
 for file_name in os.listdir(source_folder):
     file_destination = source_folder+file_name
     file_source = os.path.join(basedir,source_folder+file_name)
     storage.child("action-cards/"+file_name).put(file_source)
+    url = storage.child("property-cards/"+file_name).get_url(custom_token)
+    print(url)
+    #action_cards.append(Cards(cardId=action_card_id,))
+    #action_card_id+=1
 
-
-source_folder = "cash-cards\\"
+source_folder = "..\\monopoly-cards\\cash-cards\\"
+cash_cards = []
+cash_card_id = 1
 for file_name in os.listdir(source_folder):
     file_destination = source_folder+file_name
     file_source = os.path.join(basedir,source_folder+file_name)
     storage.child("cash-cards/"+file_name).put(file_source)
-
+    url = storage.child("property-cards/"+file_name).get_url(custom_token)
+    print(url)
