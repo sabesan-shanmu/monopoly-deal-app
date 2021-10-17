fa6b236a595b56daf8ae221e8e69bc8c4815b02e
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-10-17 01:20:59 -0400

    added check to check if player has loaded

--- monopoly-ui/src/components/templates/GameBoardTemplate.js
+++ monopoly-ui/src/components/templates/GameBoardTemplate.js
@@ -4,19 +4,25 @@ import { GameLobbyMenu } from '../organisms/GameLobbyMenu'
 import { GameStatusEnum } from '../../common/constants'
 import { GameInProgressBoard} from '../organisms/GameInProgressBoard'
 import { GameContext } from '../../context/GameContext'
+import { PlayerContext } from '../../context/PlayerContext'
 
 
 export const GameBoardTemplate = ()=> {
     const {gameState,gameDispatch} = useContext(GameContext);
+    const {playerState,playerDispatch} = useContext(PlayerContext);
 
     return (
         <React.Fragment>
-            <GameBoardHeader/>
-            {gameState.game && gameState.game?.gameStatus == GameStatusEnum.WaitingToStart &&
-                <GameLobbyMenu/>
-            }
-            {gameState.game && gameState.game?.gameStatus == GameStatusEnum.InProgress &&
-                <GameInProgressBoard/>
+            {playerState.player && 
+                <React.Fragment>
+                    <GameBoardHeader/>
+                    {gameState.game && gameState.game?.gameStatus == GameStatusEnum.WaitingToStart &&
+                        <GameLobbyMenu/>
+                    }
+                    {gameState.game && gameState.game?.gameStatus == GameStatusEnum.InProgress &&
+                        <GameInProgressBoard/>
+                    }
+                </React.Fragment>
             }
         </React.Fragment>
     )
