c6ab87cd552e3a110628a845ac5829a2bba9deaa
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-10-24 00:51:24 -0400

    missed commit

---
+++ monopoly-ui/src/components/atoms/MessageHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {getBackgroundColour} from '../../common/ImageHelpers'
+
+export const MessageHeader = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
