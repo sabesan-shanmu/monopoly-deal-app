e9f59284e3e14627093abaef6eb1950e84022f0f
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-11-20 23:15:33 -0500

    delete reference

--- monopoly-ui/src/components/atoms/CardPopoverContent.js
+++ monopoly-ui/src/components/atoms/CardPopoverContent.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import {getGameMoveName} from '../../common/GameMoveHelpers'
 
 const StyledPopoverContent = styled.div`
     width:115 px;
