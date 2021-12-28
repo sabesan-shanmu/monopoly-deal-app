9477c58659cf22c4242d3e94d06e8eab4a0c1708
sabesan-shanmu <sabesan.shanmu@gmail.com>
2021-12-28 13:18:08 -0500

    check win when player updates gamecard

--- monopoly-api/monopoly/api/games/gameCards/resource.py
+++ monopoly-api/monopoly/api/games/gameCards/resource.py
@@ -3,16 +3,17 @@ from flask_restx import Resource,Namespace
 from flask import request,jsonify
 from marshmallow import ValidationError
 from .services import get_game_cards_in_play,get_game_card_by_id,update_game_card
-from monopoly.api.games.services import get_game_by_gamepasscode
+from monopoly.api.games.services import get_game_by_gamepasscode,update_game
 from .schema import GameCardSchema,update_game_card_schema
 from monopoly.auth import validate_gamepassCode,validate_player
 from monopoly.exceptions import ResourceNotFoundException,ResourceValidationException,FieldValidationException
 from marshmallow import ValidationError
 from flask_jwt_extended import get_jwt_identity
 from monopoly.notifications.playerCards import publish_update_player_cards_on_hand_event_to_room,publish_delete_player_cards_on_hand_event_to_room
-from monopoly.common.enums import GameCardLocationStatus
+from monopoly.common.enums import GameCardLocationStatus,GameStatus
 from monopoly.notifications.games import publish_game_update_event_to_room
 from monopoly.api.games.schema import GameSchema
+from monopoly.api.games.gamePlayerMoves.services import get_winner_id
 
 game_cards_namespace = Namespace('GameCards', description='Game cards that are in play on the board')
 
@@ -63,9 +64,17 @@ class SingleGameCardsResource(Resource):
                 updated_gameCards_result = GameCardSchema(many=True).dump([new_game_card])
                 publish_delete_player_cards_on_hand_event_to_room(gamePassCode,identity["playerId"],updated_gameCards_result)
 
+            #check to see if theres a winner
+            winner_found = get_winner_id(game)
+            if winner_found:
+                #update winner
+                game.gameStatus=GameStatus.Completed
+                game.gameWinnerId= winner_found.playerId
+                update_game(game)
+                
             #publish updated game
-            update_game = get_game_by_gamepasscode(gamePassCode)
-            update_game_result = GameSchema().dump(update_game)
+            updated_game = get_game_by_gamepasscode(gamePassCode)
+            update_game_result = GameSchema().dump(updated_game)
             publish_game_update_event_to_room(gamePassCode,update_game_result)
 
             result = GameCardSchema().dump(new_game_card)
